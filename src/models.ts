7106d1d169baeb1beb0c13d2b697c2797c7deae9
ben <ben@jammerware.com>
2018-07-09 01:17:04 -0400

    Initial commit

---
+++ src/models.ts
@@ -0,0 +1,43 @@
+export class NcExtension {
+    name: string;
+    apiVersion: NcVersion;
+    extensionVersion: NcVersion;
+    costsEndpoint: string;
+    queryEndpoint: string;
+    raritiesEndpoint: string;
+}
+
+export class NcCard {
+    name: string;
+    rarity: string;
+    cost: string[];
+    types: string[];
+    subtypes: string[];
+    thumbnail: string;
+    text?: string;
+    printings?: NcPrinting[];
+}
+
+export class NcPrinting {
+    artist?: string;
+    collectorsNumber?: string;
+    flavorText?: string;
+    image: string;
+    printingIcon?: string;
+    printedIn?: string;
+    viewOn?: NcExternalInfoProvider[] = [];
+    buyAt?: NcExternalInfoProvider[] = [];
+}
+
+export class NcExternalInfoProvider {
+    url: string;
+    icon: string;
+    name: string;
+    price?: string;
+}
+
+export class NcVersion {
+    major: number;
+    minor: number;
+    patch: number;
+}
